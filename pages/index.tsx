269071edff49e378883b7d51fbf6c084317f90e6
Alan Yau <alan.yau@vicosys.com.hk>
2021-04-17 10:17:45 +0800

    style: update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,25 +30,27 @@ const IndexPage = () => {
 	const { loading, response } = useFetch({ query: JSON.stringify({ query }), url: '/api/graphql' });
 
 	return (
-		<Grid container>
-			<Grid item container justify='center' alignItems='center'>
-				<Grid item xs={12} sm={10}>
-					{loading ? (
-						<CircularProgress />
-					) : (
-						<Grid container spacing={3}>
-							{response!.getNews
-								?.filter((x: NewsType) => x.subtext?.author?.value !== 'More')
-								.map((data: any, index: number) => (
-									<Grid item xs={12} sm={6} lg={3} key={index + data.title}>
-										<CustomCard {...data} />
-									</Grid>
-								))}
-						</Grid>
-					)}
+		<div className='container'>
+			<Grid container>
+				<Grid item container justify='center' alignItems='center'>
+					<Grid item xs={12} sm={10}>
+						{loading ? (
+							<CircularProgress color='secondary' />
+						) : (
+							<Grid container spacing={3}>
+								{response!.getNews
+									?.filter((x: NewsType) => x.subtext?.comment?.value !== 'More')
+									.map((data: any, index: number) => (
+										<Grid item xs={12} sm={6} lg={3} key={index + data.title}>
+											<CustomCard {...data} />
+										</Grid>
+									))}
+							</Grid>
+						)}
+					</Grid>
 				</Grid>
 			</Grid>
-		</Grid>
+		</div>
 	);
 };
 
